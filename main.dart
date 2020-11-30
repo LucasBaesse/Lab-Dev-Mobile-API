0668a4ac2b60b381e151da7cb1fa9d50c230a684
LucasBaesse <68923233+LucasBaesse@users.noreply.github.com>
2020-11-30 16:55:47 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:webapi/views/beer.view.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Lista de Cervejas",
+    debugShowCheckedModeBanner: false,
+    home: HomeApp(),
+  ));
+}
